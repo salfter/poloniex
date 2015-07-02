3db071638c96342316deef83f2c05ea05f08098b
Scott Alfter <scott@alfter.us>
2015-07-01 22:55:10 -0700

    as fetched from http://pastebin.com/raw.php?i=8fBVpjaj...needs updates

---
+++ poloniex.py
@@ -0,0 +1,11 @@
+--2015-07-01 22:54:44--  http://pastebin.com/raw.php?i=8fBVpjaj
+Resolving pastebin.com... 190.93.243.15, 141.101.112.16, 190.93.240.15, ...
+Connecting to pastebin.com|190.93.243.15|:80... connected.
+HTTP request sent, awaiting response... 200 OK
+Length: unspecified [text/plain]
+Saving to: ‘raw.php?i=8fBVpjaj’
+
+     0K .....                                                  83.8M=0s
+
+2015-07-01 22:54:45 (83.8 MB/s) - ‘raw.php?i=8fBVpjaj’ saved [6134]
+
